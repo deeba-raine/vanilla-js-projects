6870b1b87e983ac94b08e2175788ab3099a884ca
Deeba <deeba.raine11@gmail.com>
2026-07-23 01:42:35 -0400

    test and adjust form template with error message

--- regex-form-validation/script.js
+++ regex-form-validation/script.js
@@ -1,3 +1,24 @@
 const formData = document.querySelector(".form")
-const submit = document.querySelector(".button")
+const submit = document.querySelector("#submit")
+const errorMsg = document.querySelectorAll(".error")
+let field 
+let firstName, lastName, email, password
 
+
+for (let error of errorMsg) {
+    error.classList.add("d-none");
+  
+}
+
+formData.addEventListener("keyup", (event)=> {
+    event.preventDefault()
+     field = event.target.dataset.key
+    console.log(field)
+    
+})
+
+submit.addEventListener("click", (event)=> {
+     event.preventDefault();
+    field = event.target.dataset.key
+    console.log(field)
+})
